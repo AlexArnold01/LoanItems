2091cdfb187693fc87665bacdcfa0c734044d8e4
AlexArnold01 <alar1000@stud.ek.dk>
2025-09-17 10:20:03 +0200

    Media class lavet

--- src/LoanItems/Media.java
+++ src/LoanItems/Media.java
@@ -1,4 +1,32 @@
 package LoanItems;
 
 public class Media {
+    protected String title;
+    protected String type;
+
+
+    public Media(String title, String type) {
+        this.title = title;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return title + type;
+    }
+
+    public String getTitle() {
+        return getTitle();
+    }
+    public String getType() {
+        return getType();
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
