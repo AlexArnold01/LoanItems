1b82ee0f0f457b8c7b5032ff40a4b2cde0d64913
davidek-d <dajo1002@stud.ek.dk>
2025-09-17 10:22:20 +0200

    f

--- src/LoanItems/Book.java
+++ src/LoanItems/Book.java
@@ -1,4 +1,15 @@
 package LoanItems;
 
-public class Book {
+public class Book extends Media {
+
+    //Fuld konstruktør
+    public Book(String type, String title) {
+        super(type, title);
+    }
+
+    //toString metode, @overider Media() toString.
+    @Override
+    public String toString() {
+        return "Type: " type + title;
+    }
 }
--- src/LoanItems/Video.java
+++ src/LoanItems/Video.java
@@ -1,4 +1,14 @@
 package LoanItems;
 
-public class Video {
+public class Video extends Media {
+    //Fuld konstruktør
+    public Video(String type, String title) {
+        super(type, title);
+    }
+
+    //toString metode, @overider Media() toString.
+    @Override
+    public String toString() {
+        return "Type: " type + title;
+    }
 }
